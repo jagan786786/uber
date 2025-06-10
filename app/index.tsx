6674e8e6b84b84729bc6695b1080ef9876eecf80
Jagannath <jagannathpatro234@gmail.com>
2025-06-10 23:18:56 +0530

    second commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 // app/index.js
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
+import { useAuth } from '@clerk/clerk-expo'
 
-export default function Home() {
-  return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
-  );
-}
+export const Home = () => {
+
+   const { isSignedIn } = useAuth()
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />
+  }
+  return <Redirect href="/(auth)/welcome" />;
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       fontFamily: {
@@ -82,5 +83,4 @@ module.exports = {
     },
   },
   plugins: [],
-  presets: [require("nativewind/preset")],
 };
